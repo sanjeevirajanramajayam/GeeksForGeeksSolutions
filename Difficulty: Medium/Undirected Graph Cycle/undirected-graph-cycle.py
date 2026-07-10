d296e6e1af56772df71ea1cf0a94902d9f519623
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-10 18:43:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.py
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.py
@@ -2,32 +2,31 @@ from collections import deque
 class Solution:
 	def isCycle(self, V, edges):
 		#Code here
-		adjList = [[] for i in range(V + 1)]
+		def bfs(node, parent):
+		    queue = deque([(node, parent)])
+		    while queue:
+		       node, parent = queue.popleft()
+		       for i in adjList[node]:
+		          # bfs(i, node)
+		          if i != parent and i in visited:
+		              return True
+		          
+		          if i not in visited:
+		              #bfs(i, node)
+		              queue.append((i, node))
+		              visited.add(i)
+		    return False
+		    
+		adjList = [[] for i in range(V)]
+		visited = set([])
+		for startNode, endNode in edges:
+		    adjList[startNode].append(endNode)
+		    adjList[endNode].append(startNode)
 		
-		for i in range(1, len(edges)):
-		    adjList[edges[i][0]].append(edges[i][1])
-		    adjList[edges[i][1]].append(edges[i][0])
-		    
-		visited = [0 for i in range(V + 1)]
-		    
-		def bfs(givenNode):
-    		queue = deque([(givenNode, -1)])
-    		visited[givenNode] = 1
-
-    		while queue:
-    		    node, parent = queue.popleft()
-    		    for neighbour in adjList[node]:
-    		        if neighbour != parent and visited[neighbour] == 1:
-    		          #  print(node, neighbour, parent)
-    		            return True
-    		        if not visited[neighbour]:
-        		        queue.append((neighbour, node))
-        		        visited[neighbour] = 1
-        	return False
-        
-        for i in range(1, V + 1):
-            if not visited[i]:
-                if bfs(i):
-                    return True
-	
-        return False
+		for i in range(V):
+		    if i not in visited:
+		        visited.add(i)
+		        if bfs(i, -1) == True:
+		            return True
+		
+		return False
