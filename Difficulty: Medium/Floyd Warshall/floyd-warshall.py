ee962715f76bb592ec7daaa8f58086ececedfa29
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-14 18:16:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.py
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.py
@@ -1,18 +1,13 @@
-#User function template for Python
-
 class Solution:
-	def floydWarshall(self, dist):
-		#Code here
-		for i in range(len(dist)):
-            for j in range(len(dist)):
-		        if dist[i][j] == 100000000:
-		            dist[i][j] = float('inf')
-		for via in range(len(dist)):
-		    for i in range(len(dist)):
-		        for j in range(len(dist)):
-		            dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j])
-		for i in range(len(dist)):
-            for j in range(len(dist)):
-		        if dist[i][j] == float('inf'):
-		            dist[i][j] = 100000000
-	    return dist
+    def floydWarshall(self, dist):
+        n = len(dist)
+        INF = 100000000
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][k] != INF and dist[k][j] != INF:
+                        dist[i][j] = min(dist[i][j],
+                                         dist[i][k] + dist[k][j])
+
+        return dist
