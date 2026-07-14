4ddb33476a0804c0d799406cdf96af78780168e3
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-14 11:09:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.py
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.py
@@ -1,27 +1,26 @@
-import heapq
+from collections import deque
 class Solution:
     # Returns shortest distances from src to all other vertices
     def dijkstra(self, V, edges, src):
         # code here
-        adjList = [[] for i in range((V))]
-        for startNode, endNode, weight in edges:
-            adjList[startNode].append((endNode, weight))
-            adjList[endNode].append((startNode, weight))
-        
-        pq = []
-        heapq.heappush(pq, (0, src))
-        
-        distList = [float('inf') for i in range(V)]
-        distList[src] = 0
-        
+        adjList = [[] for i in range(V)]
+        for startNode, endNode, wt in edges:
+            adjList[startNode].append((endNode, wt))
+            adjList[endNode].append((startNode, wt))
+        pq = [(0, src)]
+        dist = [float('inf')] * V
+        dist[src] = 0
         while pq:
-            dist, node = heapq.heappop(pq)
-            if dist > distList[node]:
+            d, node = heapq.heappop(pq)
+            if d > dist[node]:
                 continue
-            for newNode, weight in adjList[node]:
-                # print(dist, weight, distList[newNode])
-                if dist + weight < distList[newNode]:
-                    distList[newNode] = dist + weight
-                    heapq.heappush(pq, (distList[newNode], newNode))
-        
-        return distList
+            for nei, wt in adjList[node]:
+                # print(nei, wt)
+                if d + wt < dist[nei]:
+                    dist[nei] = d + wt
+                    heapq.heappush(pq, (d + wt, nei))
+                    # print(dist)
+            # print(pq)
+        # return 
+        # print(dist)
+        return dist
