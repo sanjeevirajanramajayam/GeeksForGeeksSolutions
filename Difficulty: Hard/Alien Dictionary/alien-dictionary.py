ed793f1758837dc25f193dc279266b3833307358
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-07-16 12:02:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.py
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.py
@@ -3,29 +3,28 @@ class Solution:
     def findOrder(self, words):
         # code here
         inorder = {}
-        adjList = defaultdict(list)
-        
         for word in words:
             for ch in word:
                 inorder[ch] = 0
+        adjList = defaultdict(list)
         
-        for i in range(len(words) - 1):
-            word1 = words[i]
-            word2 = words[i + 1]
+        for i in range(1, len(words)):
+            word2 = words[i]
+            word1 = words[i-1]
             
             if len(word1) > len(word2):
-                same = True
+                flag = True
                 j = 0
                 while j < len(word2):
                     if word1[j] != word2[j]:
-                        same = False
+                        flag = False
                         break
                     j += 1
-                if same:
+                if flag:
                     return ""
-                    
+        
             length = min(len(word1), len(word2))
-            j=0
+            j = 0
             while j < length:
                 if word1[j] != word2[j]:
                     if word2[j] not in adjList[word1[j]]:
@@ -33,26 +32,24 @@ class Solution:
                         inorder[word2[j]] += 1
                     break
                 j += 1
+        
         # print(adjList, inorder)
-        # return ""
-        visited = set()
+        # return "abcd"
         queue = deque([])
-        for x in inorder:
-            if inorder[x] == 0:
-                queue.append(x)
-                # visited.add(x)
-        ans = []
+        for i in inorder:
+            if inorder[i] == 0:
+                queue.append(i)
+        # queue = deque()
+        ans = ""
         while queue:
-            node = queue.popleft()
-            ans.append(node)
-            for x in adjList[node]:
-                # if x not in visited:
-                inorder[x] -= 1
-                if inorder[x] == 0:
-                    queue.append(x)
-                    # visited.add(x)
+            node = queue.popleft()                    
+            ans += (node)
+
+            for nnode in adjList[node]:
+                inorder[nnode] -= 1
+                if inorder[nnode] == 0:
+                    queue.append(nnode)
+        # print(ans)
         if len(ans) != len(inorder):
             return ""
-        # print(ans)
-        return "".join(ans)
-        
+        return ans
