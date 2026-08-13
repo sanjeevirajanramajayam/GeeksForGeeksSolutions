e8ff48d2de0c51cdf852cef6340870d1067e53cb
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-13 14:19:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.py
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.py
@@ -1,20 +1,15 @@
 class Solution:
-    def longCommSubstr(self, text1, text2):
+    def longCommSubstr(self, s1, s2):
         # code here
-        dp = [[-1 for i in range(len(text2) + 1 )] for j in range(len(text1) + 1 )]
-
-        for i in range(len(text1) + 1):
-            dp[i][0] = 0
-        for i in range((len(text2) + 1)):
-            dp[0][i] = 0
-        ans = 0
-        for i in range(1, len(text1) + 1):
-            for j in range(1, len(text2) + 1):
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+        dp = [[0 for i in range(len(s2) + 1)] for i in range(len(s1) + 1)]
+        ans = float('-inf')
+        for i in range(1, len(s1) + 1):
+            for j in range(1, len(s2) + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
                     ans = max(ans, dp[i][j])
-                    continue
-                dp[i][j] = 0
-
-
+                else:
+                    dp[i][j] = 0
+        if ans == float('-inf'):
+            return 0
         return ans
