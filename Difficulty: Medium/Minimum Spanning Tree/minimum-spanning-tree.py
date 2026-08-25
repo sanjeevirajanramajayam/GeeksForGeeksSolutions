0c7ed75680e49773dcdd3d22764d4482575d470e
sanjeevirajanramajayam <163754159+sanjeevirajanramajayam@users.noreply.github.com>
2026-08-25 23:16:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.py
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.py
@@ -1,31 +1,20 @@
-import heapq
 class Solution:
-    def spanningTree(self, V, edges):
-        # code here
-        visited = [ 0 for i in range(V)]
-        mst_edges = []
+    def spanningTree(self, V: int, edges: list[list[int]]) -> int:
         pq = [(0, 0, -1)]
-        totalWt = 0
+              #wt  #node    #parent    
+        visited = set()
+        ans = 0
         adjList = [[] for i in range(V)]
-        
         for startNode, endNode, wt in edges:
             adjList[startNode].append((endNode, wt))
             adjList[endNode].append((startNode, wt))
-            
+
         while pq:
             wt, node, parent = heapq.heappop(pq)
-            
-            if visited[node] == 1:
+            if node in visited:
                 continue
-            
-            visited[node] = 1
-            totalWt += wt
-            
-            if parent != -1:
-                mst_edges.append((parent, node, wt))
-            
-            for newNode, dist in adjList[node]:
-                if visited[newNode] != 1:
-                    heapq.heappush(pq, (dist, newNode, node))
-        # print(mst_edges)
-        return totalWt
+            ans += wt
+            visited.add(node)
+            for nnode, nwt in adjList[node]:
+                heapq.heappush(pq, (nwt, nnode, node))
+        return ans
